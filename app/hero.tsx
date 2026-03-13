b0a13ab24f2268ee9f25ce5473fec6d84b045537
clement Roland <clementroland52@gmail.com>
2026-03-13 13:14:13 -0400

    Enhance Hero component with new layout and content, including title, description, and button for user interaction.

--- app/hero.tsx
+++ app/hero.tsx
@@ -5,8 +5,17 @@ export default function Hero(){
 
     return(
         <>
-        <div>
-            <img src={hero.src} className='w-full h-full object-cover' />
+        <div className="relative mt-43">
+            <img src={hero.src} className="w-full h-full object-cover" />
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-center ">
+                <h1 className="text-4xl font-bold text-white mb-4">KaruFoods</h1>
+                <p className="text-2xl text-white max-w-xl capitalize mb-6">
+                    Le Gouts des Caraibes Burgers, bokits et snacks délicieux
+                </p>
+                <button className="bg-sky-400 hover:bg-sky-500 text-white text-sm px-4 py-2 md:text-lg md:px-8 md:py-4 rounded-full shadow-md">
+                    Commander maintenant
+                </button>
+            </div>
         </div>
         </>
     )
